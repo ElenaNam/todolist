797432237d2b2e7b860edb95684aab74a56e0440
Elena Kirillova <el-ki-ri@yandex.ru>
2025-06-21 23:38:55 +0400

    add base api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
+import { useMeQuery } from "@/features/auth/api/authApi";
+
 export const App = () => {
+	const {data, isLoading, isError, error} = useMeQuery()
+
+
+	if(isLoading) return <p>Loading...</p> 
+	if(isError) return <pre>{JSON.stringify(error, null, 2)}</pre> //TODO: handleError
+
 	return (
 		<div className="App">
-			App
+			{data?.data.login}
 		</div>
 	);
 }
---
+++ src/features/auth/api/authApi.ts
@@ -0,0 +1,18 @@
+import { baseApi } from "@/app/baseApi"
+import { BaseResponse } from "@/common/types/types"
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    me: build.query<BaseResponse<{ id: number; login: string; email: string }>, void>({
+      query: () => "auth/me",
+      transformResponse: (response: BaseResponse<{ id: number; login: string; email: string }>) => {
+        if (response.resultCode !== 0) {
+          throw new Error(response.messages?.[0] || "Unknown error")
+        }
+        return response
+      },
+    }),
+  }),
+})
+
+export const { useMeQuery } = authApi
